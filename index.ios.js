b4ca0af9f158410c591bfaae93251997207d71c7
Tony <gibbsc@indiana.edu>
2016-07-27 08:47:05 -0400

    first commit

---
+++ index.ios.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  MapView
+} from 'react-native';
+
+class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Image
+        style={styles.pic}
+          source={require('./img/logo.png')}
+        />
+        <MapView
+       style={{height: 200, width: 400}}
+       showsUserLocation={true}
+     />
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  pic: {
+    width:80,
+    height:80,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
